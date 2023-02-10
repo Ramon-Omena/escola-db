0a8155818e213f36a8e6e461d58d2e519050328a
Ramon-Omena <118060028+Ramon-Omena@users.noreply.github.com>
2023-02-10 00:47:09 -0300

    Add files via upload

---
+++ AcademiaDB.sql
@@ -0,0 +1,51 @@
+create database Academia;
+use Academia;
+
+CREATE TABLE modalidades (
+    id_modalidade int AUTO_INCREMENT PRIMARY KEY,
+    nome_modalidade varchar(30) NOT NULL,
+    nome_instrutor varchar(30) NOT NULL
+);
+
+CREATE TABLE alunos (
+    id_alunos int AUTO_INCREMENT PRIMARY KEY,
+    nome_alunos varchar(30) NOT NULL,
+    modalidade_id INT,
+    CONSTRAINT FOREIGN KEY (modalidade_id) REFERENCES modalidades (id_modalidade)
+);
+
+insert into modalidades(nome_modalidade, nome_instrutor) values ('Musculação', 'Fábio Farias');
+insert into modalidades(nome_modalidade, nome_instrutor) values ('Ginástica', 'Rose Ronalsa');
+insert into modalidades(nome_modalidade, nome_instrutor) values ('Zumba', 'Maria Mercedes');
+insert into modalidades(nome_modalidade, nome_instrutor) values ('CrossFit', 'Antônio Antunes');    
+
+insert into alunos(nome_alunos, modalidade_id) values ('Walter', 1);
+insert into alunos(nome_alunos, modalidade_id) values ('Wesley', 1);
+insert into alunos(nome_alunos, modalidade_id) values ('Wanda', 2);
+insert into alunos(nome_alunos, modalidade_id) values ('Wanessa', 3);
+insert into alunos(nome_alunos, modalidade_id) values ('Waldomiro', NULL);
+insert into alunos(nome_alunos, modalidade_id) values ('Waleria', NULL);
+
+select nome_alunos, nome_modalidade from alunos
+inner join modalidades
+on modalidades.id_modalidade = alunos.modalidade_id;
+
+select nome_alunos, nome_modalidade
+from
+alunos left join modalidades
+on modalidades.id_modalidade = alunos.modalidade_id;
+
+select nome_alunos, nome_modalidade
+from
+alunos right join modalidades
+on modalidades.id_modalidade = alunos.modalidade_id;
+
+select nome_alunos, nome_modalidade
+from
+alunos left join modalidades
+on modalidades.id_modalidade = alunos.modalidade_id;
+union
+select nome_alunos, nome_modalidade
+from
+alunos right join modalidades
+on modalidades.id_modalidade = alunos.modalidade_id;
