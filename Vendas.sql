4ffc58ef2251b24fab7279702f22b07199e31bb8
Ramon-Omena <118060028+Ramon-Omena@users.noreply.github.com>
2023-02-11 00:46:54 -0300

    Update Vendas.sql

--- Vendas.sql
+++ Vendas.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE TRG_total_de_vendas_diaria (IN produto VARCHAR(30))
 BEGIN
   SELECT CONCAT('Hoje o total de vendas desse produto, foi: ', totalVenda) AS totalVenda
   FROM tabela_vendas
-  WHERE vendas_produto = produto;
+  WHERE nome_produto = produto;
 END$$
-DELIMITER
+DELIMITER;
   
