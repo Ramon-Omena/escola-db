421eba2f6caec3d0e27dfda69206c5e8c479e51c
Ramon-Omena <118060028+Ramon-Omena@users.noreply.github.com>
2023-02-11 00:36:30 -0300

    Create Vendas.sql

---
+++ Vendas.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE PROCEDURE TRG_total_de_vendas_diaria (IN produto VARCHAR(30))
+BEGIN
+  SELECT CONCAT('Hoje o total de vendas desse produto, foi: ', totalVenda) AS totalVenda
+  FROM tabela_vendas
+  WHERE vendas_produto = produto;
+END$$
+DELIMITER
+  
