011aae0acab350601eaceedf8756a1e4a5af8e8a
Ramon-Omena <118060028+Ramon-Omena@users.noreply.github.com>
2023-02-10 21:29:10 -0300

    Update AcademiaDB.sql

--- AcademiaDB.sql
+++ AcademiaDB.sql
@@ -10,8 +10,15 @@ CREATE TABLE modalidades (
 CREATE TABLE alunos (
     id_alunos int AUTO_INCREMENT PRIMARY KEY,
     nome_alunos varchar(30) NOT NULL,
-    modalidade_id INT,
+    modalidade_id INT NULL,
     CONSTRAINT FOREIGN KEY (modalidade_id) REFERENCES modalidades (id_modalidade)
+);
+
+CREATE TABLE aviso (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    mensagem VARCHAR(50) NOT NULL,
+    aluno_avisado_id INT,
+    CONSTRAINT FOREIGN KEY (aluno_avisado_id) REFERENCES alunos (id_alunos)
 );
 
 insert into modalidades(nome_modalidade, nome_instrutor) values ('Musculação', 'Fábio Farias');
@@ -49,3 +56,15 @@ select nome_alunos, nome_modalidade
 from
 alunos right join modalidades
 on modalidades.id_modalidade = alunos.modalidade_id;
+
+--CRIANDO TRIGGER
+CREATE TRIGGER aluno_matriculado AFTER INSERT
+ON alunos
+FOR EACH ROW
+BEGIN
+    IF NEW.modalidade_id IS NULL THEN
+        INSERT INTO aviso(aluno_avisado_id, mensagem) VALUES
+        (NEW.id_aluno, CONCAT('NEW.nome_alunos', selecione uma modalidade.'));
+    END IF;
+END
+
